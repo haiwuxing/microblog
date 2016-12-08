c98f72c8e0e0df30627d3687e5f5708092138867
Li Jian <haiwuxing@gmail.com>
2016-12-09 06:29:39 +0800

    备份

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+# tell Flask to read it and use config.
+app.config.from_object('config')
 
 # 此句一定要在 app 创建以后调用，因为views.py 中用到了app
 from app import views
---
+++ config.py
@@ -0,0 +1,2 @@
+WTF_CSRF_ENABLED = True; # activates the cross-site request forgery prevention
+SECRET_KEY = 'you-will-never-guess'; # only needed when CSRF is enabled
