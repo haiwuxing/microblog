e02ebecb5f7a7819c6fe00d342831bcaea3f7866
Li Jian <lijian@hantek.com>
2016-12-08 15:34:49 +0800

    Part II: Templates

--- app/views.py
+++ app/views.py
@@ -1,8 +1,21 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!";
-	
-	
+    user = {'nickname': 'Miguel'}; # fake user
+    posts = [ # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beutiful day in Porland!'
+            },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avergers movie was so cool!'
+            }
+        ]
+    return render_template('index.html',
+                        title='Home',
+                        user=user,
+                        posts=posts);
